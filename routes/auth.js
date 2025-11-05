89069c6bfe0e358c6aaa95d459b8e01a55d52ee5
Aritra Karan <karanaritra19@gmail.com>
2025-11-05 12:55:11 +0530

    error fixed

--- routes/auth.js
+++ routes/auth.js
@@ -2,23 +2,29 @@ const express = require ('express');
 const User = require('../models/user');
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const user = require('../models/user');
 
 router.post('/',[
-    body('name').isLength({min: 5}),
-    body('email').isEmail(),
+    body('name','Enter a valid Name').isLength({min: 5}),
+    body('email','Enter a valid email').isEmail(),
     body('password').isLength({min:5})
     
-],(req,res)=>{
+], (req,res)=>{
     // console.log(req.body);
     
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
-    }else{
-        const user = User(req.body);
-        user.save();
-        res.send(req.body);
     }
+    User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    }).then(user => res.json(user))
+    .catch(err=>{console.log(err)
+        res.json({error: "please enter a unique value"})
+    })
+    
     
 });
 module.exports = router
